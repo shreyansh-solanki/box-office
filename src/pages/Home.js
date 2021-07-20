d8c47465412c58d58e3548ab748dd801313c4119
shreyansh <shreyansh1029@gmail.com>
2021-07-20 15:09:51 +0530

    displayed show results and small fetch refactor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,43 @@
 /* eslint-disable no-console */
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
     // eslint-disable-next-line no-unused-vars
     const [input, setInput] = useState('');
+    const [results, setResults] = useState(null);
 
     const onInputChange = ev => {
         setInput(ev.target.value);
     };
 
     const onSearch = () => {
-        fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-            .then(r => r.json())
-            .then(result => {
-                console.log(result);
-            });
+        apiGet(`/search/shows?q=${input}`).then(result => {
+            setResults(result);
+        });
     };
 
     const onKeyDown = ev => {
         if (ev.keyCode === 13) {
             onSearch();
         }
+    };
+
+    const renderResults = () => {
+        if (results && results.length === 0) {
+            return <div>No result</div>;
+        }
+        if (results && results.length > 0) {
+            return (
+                <div>
+                    {results.map(item => (
+                        <div key={item.show.id}>{item.show.name}</div>
+                    ))}
+                </div>
+            );
+        }
+        return null;
     };
 
     return (
@@ -35,6 +51,7 @@ const Home = () => {
             <button type="button" onClick={onSearch}>
                 Search
             </button>
+            {renderResults()}
         </MainPageLayout>
     );
 };
